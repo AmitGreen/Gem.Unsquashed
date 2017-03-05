41ad613109b78b8bd17422cc7a4cb2f9c13811c8
Amit Green <amit.mixie@gmail.com>
2017-03-05 15:29:02 -0500

    TREMOLITE: Squash of 29 commits:
        Added Tremolite/Compile.py to compile a parsed regular expression
        Add encode_ascii & use in regular expression parsing -- renamed parse_regular_expression to parse_ascii_regular_expression; changed return type of parse_ascii_regular_expression to a string or [flat] tuple & used this return tyope in def compile_regular_expression
        Start code on to build regular expressions
        Start creating more Tremolite classes (will be renamed in a later commit)
        Get Regular expressions working -- Squash of the following 22 commits:
            Minor cleanups: converting from portray_raw_string to portray_string (and also make both portray_raw_string & portray_string builtins)
            Remove redudant code in Tremolite/Build.py b/Tremolite/Build.py
            Allow strings to be used in Tremolite expressions; split TremoliteExact to Tremolite{Multiple,Singular}; created TremoliteGroup; clean up generation of Ascii
            Cleanup Tremolite objects (add .portray & creating invisible EXACT to simplify rest of code.  Also rewrote TremoliteBase.__radd__ to use .__add__)
            More cleanup of Tremolite classes, now all havee .portray member (and remove redudant TremoliteMultiple); Also Create intern_arrange
            Change .__class_ to type
            Start using built tremolite expresssion with .compile_ascii_regular_expression (also minor spelling error fixes)
            Create Tremolite function ANY; Rename WRAP to wrap_string (and only call for strings); fix compile_regular_expression with two or more groups
            Add | operator to Tremolite patterns
            Add TremoliteRepeat & implement for ? (also created & used internal function wrap_parenthesis)
            Add ONE_OR_MORE & deal with a repeat count > 255
            Rewrite parse_ascii_regular_expression to always return a tuple of three items (and also change compile_regular_expression to match)
            Add ZERO_OR_MORE, work on making parenthesis invisible (will be removed & optimized in next commit)
            Add function create_repeat (and use in ONE_OR_MORE OPTIONAL, ZERO_OR_ONE); sort functions in TremoliteBuild.py; Rename TremoliteAny to TremoliteAnyof (and ANY to ANY_OF); Only show one string in Tremolite{Multiple,Singular} if both strings the same.
            Add EMPTY for Tremolite empty patterns
            Cleanup TremoliteEmpty (merged to TremoliteSpecial); Merged Tremolite{Multiple,Singular} to TremoliteExact; Renamed TremoliteSpecialSingular to TremoliateSpecial; also cleaned up Tremolite unit tests
            Rename .pattern to .regular_expression in Tremolite code; start adding TremoliteMatch.py
            Rename Tremolite function wrap_string as INVISIBLE_EXACT.
            Moved Tremolite testing to Topaz/Pattern.py; fixed spelling of produce_cache_and_insert_function
            Start work on Tremolite/Main.py
            Tremolite code is now generating Tremolite/Match.py (also removed trailing spaces on blank lines)
            Use copyright from Tremolite/Parse.py for generated code; Add Tremolite.ParsedMatch & use in GROUP function
        Added Tremolite/LicenseTemplate.txt & also started to deal with difference between python_2 & python_3 generated output
        Generated Tremolite code now detects python version & falls back to normal compiling regular expressions; Also cleaned up Gem/Import.py; Change regular expression for groups to put '_' before 'a-z'.
        Minor cleanups & partial work on copyrights (will finish later & squash)

--- Tremolite/Core.py
+++ Tremolite/Core.py
@@ -3,14 +3,41 @@
 #
 @gem('Tremolite.Core')
 def gem():
+    require_gem('Gem.Ascii')
+    require_gem('Gem.Cache')
+    require_gem('Gem.Codec')
+    require_gem('Gem.DelayedFileOutput')
+    require_gem('Gem.Exception')
     require_gem('Gem.Import')           #   For import_module
     require_gem('Gem.Map')
+    require_gem('Gem.Path')
+    require_gem('Gem.PortrayString')
+    require_gem('Gem.System')
 
 
-    from Gem import first_map_item, view_items
+    from Gem import create_DelayedFileOutput, encode_ascii, first_map_item
+    from Gem import iterate_items_sorted_by_key, iterate_values_sorted_by_key
+    from Gem import lookup_ascii, produce_cache_and_insert_function, python_version, read_text_from_path, view_items
 
 
     share(
-        'first_map_item',       first_map_item,
-        'view_items',           view_items,
+        #
+        #   Imported functions
+        #
+        'create_DelayedFileOutput',             create_DelayedFileOutput,
+        'encode_ascii',                         encode_ascii,
+        'first_map_item',                       first_map_item,
+        'iterate_items_sorted_by_key',          iterate_items_sorted_by_key,
+        'iterate_values_sorted_by_key',         iterate_values_sorted_by_key,
+        'lookup_ascii',                         lookup_ascii,
+        'produce_cache_and_insert_function',    produce_cache_and_insert_function,
+        'read_text_from_path',                  read_text_from_path,
+        'view_items',                           view_items,
+
+
+        #
+        #   Values
+        #
+        'list_of_single_none',  [none],
+        'python_version',       python_version,
     )
