373b78ae135c4c7ffa82cce453aa1558a65fbfaa
Amit Green <amit.mixie@gmail.com>
2017-03-29 00:02:17 -0400

    whatever

--- Topaz/Main.py
+++ Topaz/Main.py
@@ -13,16 +13,23 @@ def boot():
     from sys     import path    as module_path
     from os.path import abspath as path_absolute, join as path_join
 
-    module_path.insert(1, path_absolute(path_join(module_path[0    ], '..')))
+    module_path.insert(1, path_absolute(path_join(module_path[0], '..')))
 
     import Gem
 
 
 @gem('Topaz.Main')
 def gem():
+    require_gem('Gem.DelayedFileOutput')
+    require_gem('Gem.Path')
     require_gem('Topaz.PortrayString')
+    require_gem('Topaz.StringOutput')
+
+
+    from Gem import create_DelayedFileOutput, remove_path__ignore_file_not_found
 
 
     @share
     def main():
         test_portray_raw_string()
+        test_string_output()
