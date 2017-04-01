9ceff25ef04b716460f8296647d68c6798068b80
Amit Green <amit.mixie@gmail.com>
2017-04-01 09:51:12 -0400

    SAPPHIRE: Squash of 29 commits (Initial Sapphire code to parse Sapphire/Main.py):
        Initial Sapphire code
        Added Sapphire/Parse.py
        Start Sapphire/Pattern.py
        Start generating Sapphire/Match.py
        Start parsing lines of Gem code
        Start parsing empty lines, empty comment & comments
        Start parsing a define header
        Start adding indentation
        Add Return Expression; also cleanup of DefineHeader
        Rewrite parsing to only parse first keyword or comment on line & then call another function to parse rest of line; Also generate Sapphire/Match.py before calling parse_python_from_path
        Split off code from Sapphire/Parse.py to Sapphire/{Token,Statement}.py
        Parse function definitions with 0 parameters
        Prepare to parse decorators (@)
        Sapphire: Parse DecoratorHeader; Add FunctionCall_1 & initial parsing of SingleQuote; refactore parse_expression; also improved some debugging; Moved statement tokens from Sapphire/Token.py to Sapphire/Statement.py (and organized classes alphabetically)
        Added StatementFromImport (and AsFragment); also sorted parse routines
        Sapphire/Expression.py: Add ExpressionComma, move SingleQuote & Symbol here from Sapphire/Token.py; parse StatementFromImport that has a comma (,)
        Add StatementImport.  Also add StatementDot & fix previous checking for StatementFromImport.  Turn off debugging.
        Create Sapphire/ParseExpression.py: Starting to parse first argument of function call; use normal string index for function lookup; Create Sapphire/Elemental.py (and move keyword, operator & Symobl from Sapphire/{Expression,Token}.py)
        Incomplete call to parse nested function call
        Fully parse first complex nested function (and method) call.
        More incomplete code for function call parsing
        Almost finished fixing parsing of function call
        Now parses function call with 0 arguments
        Start parsing newlines
        Properly parse call statement's with indentation & newline (start using test.py for testing the parser)
        Parse & store newline in all statements (also removed EmptyComment)
        Write result parse tree & compare with original input
        Finish code to output parse tree identicial to input for Sapphire/Main.py
        Finish all .write methods to output Sapphire/Main.py parse tree

---
+++ Sapphire/Statement.py
@@ -0,0 +1,330 @@
+#
+#   Copyright (c) 2017 Amit Green.  All rights reserved.
+#
+@gem('Sapphire.Statement')
+def gem():
+    require_gem('Sapphire.Token')
+
+
+    @share
+    class AsFragment(Object):
+        __slots__ = ((
+            'left_name',                #   String+
+            'keyword_as',               #   KeywordAs
+            'right_name',               #   String+
+        ))
+
+
+        def __init__(t, left_name, keyword_as, right_name):
+            t.left_name  = left_name
+            t.keyword_as = keyword_as
+            t.right_name = right_name
+
+
+        def __repr__(t):
+            return arrange('<AsFragment %s %s %s>', t.left_name, t.keyword_as, t.right_name)
+
+
+        def write(t, w):
+            w(t.left_name + t.keyword_as.s + t.right_name)
+
+
+    @share
+    class Comment(Object):
+        __slots__ = ((
+            'comment',                  #   Comment
+            'newline',                  #   String
+        ))
+
+
+        def __init__(t, comment, newline):
+            t.comment = comment
+            t.newline = newline
+
+
+        def __repr__(t):
+            if t.comment is '':
+                return arrange('<# %r>', t.newline)
+
+            return arrange('<# %r %r>', t.comment, t.newline)
+
+
+        def write(t, w):
+            w('#' + t.comment + t.newline)
+
+
+    @share
+    class DecoratorHeader(Object):
+        __slots__ = ((
+            'operator_decorator',       #   OperatorAtSign
+            'expresssion',              #   Any
+            'newline',                  #   String
+        ))
+
+
+        def __init__(t, operator_decorator, expresssion, newline):
+            t.operator_decorator = operator_decorator
+            t.expresssion        = expresssion
+            t.newline            = newline
+
+
+        def  __repr__(t):
+            return arrange('<DecoratorHeader %r %r %r>', t.operator_decorator, t.expresssion, t.newline)
+
+
+        def write(t, w):
+            w(t.operator_decorator.s)
+            t.expresssion.write(w)
+            w(t.newline)
+
+
+
+    @share
+    class DefineHeader(Object):
+        __slots__ = ((
+            'keyword_define',           #   KeywordDefine
+            'name',                     #   String
+            'parameters_colon',         #   Parameter_0 | Parameter_1
+            'newline',                  #   String
+        ))
+
+
+        def __init__(t, keyword_define, name, parameters_colon, newline):
+            t.keyword_define   = keyword_define
+            t.name             = name
+            t.parameters_colon = parameters_colon
+            t.newline          = newline
+
+
+        def  __repr__(t):
+            return arrange('<DefineHeader %s %s %r %r>', t.keyword_define, t.name, t.parameters_colon, t.newline)
+
+
+        def write(t, w):
+            w(t.keyword_define.s + t.name)
+            t.parameters_colon.write(w)
+            w(t.newline)
+
+
+    @share
+    class EmptyLine(Token):
+        __slots__ = (())
+
+
+        def __repr__(t):
+            if t.s is '':
+                return '<EmptyLine>'
+
+            return arrange('<EmptyLine %r>', t.s)
+
+
+    @share
+    class IndentedComment(Object):
+        __slots__ = ((
+            'indented',                 #   String
+            'comment',                  #   Comment
+            'newline',                  #   String
+        ))
+
+
+        def __init__(t, indented, comment, newline):
+            t.indented = indented
+            t.comment  = comment
+            t.newline  = newline
+
+
+        def __repr__(t):
+            if t.comment is '':
+                return arrange('<+# %r %r>', t.indented, t.newline)
+
+            return arrange('<+# %r %r %r>', t.indented, t.comment, t.newline)
+
+
+        def write(t, w):
+            w(t.indented + '#' + t.comment + t.newline)
+
+
+    @share
+    class ParameterColon_0(Token):
+        pass
+
+
+    @share
+    class ParameterColon_1(Object):
+        __slots__ = ((
+            'left_parenthesis',             #   OperatorLeftParenthesis
+            'argument_1',                   #   Expression*
+            'right_parenthesis__colon',     #   OperatorRightParenthesis
+        ))
+
+
+        def __init__(t, left_parenthesis, argument_1, right_parenthesis__colon):
+            assert left_parenthesis        .is_left_parenthesis
+            assert type(argument_1) is not String
+            assert right_parenthesis__colon.is__right_parenthesis__colon
+
+
+            t.left_parenthesis         = left_parenthesis
+            t.argument_1               = argument_1
+            t.right_parenthesis__colon = right_parenthesis__colon
+
+
+        def  __repr__(t):
+            return arrange('<ParameterColon_1 %s %s %s>',
+                           portray_string(t.left_parenthesis),
+                           t.argument_1,
+                           portray_string(t.right_parenthesis__colon))
+
+
+        def write(t, w):
+            t.left_parenthesis        .write(w)
+            t.argument_1              .write(w)
+            t.right_parenthesis__colon.write(w)
+
+
+    @share
+    class StatementCall(Object):
+        __slot__ = ((
+            'indented',                 #   String+
+            'left',                     #   Expression
+            'arguments',                #   Arguments*
+            'newline',                  #   String+
+        ))
+
+
+        def __init__(t, indented, left, arguments, newline):
+            t.indented  = indented
+            t.left      = left
+            t.arguments = arguments
+            t.newline   = newline
+
+
+        def __repr__(t):
+            return arrange('<StatementCall %r %r %r %r>', t.indented, t.left, t.arguments, t.newline)
+
+
+        def write(t, w):
+            w(t.indented)
+            t.left     .write(w)
+            t.arguments.write(w)
+            w(t.newline)
+
+
+    @share
+    class StatementFromImport(Object):
+        __slots__ = ((
+            'keyword_from',             #   KeywordFrom
+            'module',                   #   String+
+            'keyword_import',           #   KeywordImport
+            'imported',                 #   String+ | AsFragment
+            'newline',                  #   String+
+        ))
+
+
+        def __init__(t, keyword_from, module, keyword_import, imported, newline):
+            assert type(module) is not String
+
+            t.keyword_from   = keyword_from
+            t.module         = module
+            t.keyword_import = keyword_import
+            t.imported       = imported
+            t.newline        = newline
+
+
+        def __repr__(t):
+            return arrange('<StatementFrom %r %r %r %r %r>',
+                           t.keyword_from, t.module, t.keyword_import, t.imported, t.newline)
+
+
+        def write(t, w):
+            w(t.keyword_from.s)
+            t.module.write(w)
+            w(t.keyword_import.s)
+            t.imported.write(w)
+            w(t.newline)
+
+
+    @share
+    class StatementMethodCall(Object):
+        __slot__ = ((
+            'indented',                 #   String+
+            'left',                     #   Expression
+            'dot',                      #   OperatorDot
+            'right',                    #   Symbol
+            'arguments',                #   Arguments*
+            'newline',                  #   String+
+        ))
+
+
+        def __init__(t, indented, left, dot, right, arguments, newline):
+            t.indented  = indented
+            t.left      = left
+            t.dot       = dot
+            t.right     = right
+            t.arguments = arguments
+            t.newline   = newline
+
+
+        def __repr__(t):
+            return arrange('<StatementCall %r %r %r %r %r %r>',
+                           t.indented, t.left, t.dot, t.right, t.arguments, t.newline)
+
+
+        def write(t, w):
+            w(t.indented)
+            t.left.write(w)
+            w(t.dot + t.right)
+            t.arguments.write(w)
+            w(t.newline)
+
+
+    @share
+    class StatementImport(Object):
+        __slots__ = ((
+            'keyword_import',           #   KeywordImport
+            'module',                   #   String+
+            'newline',                  #   String+
+        ))
+
+
+        def __init__(t, keyword_import, module, newline):
+            assert type(module) is not String
+
+            t.keyword_import = keyword_import
+            t.module         = module
+            t.newline        = newline
+
+
+        def __repr__(t):
+            return arrange('<StatementImport %r %r %r>', t.keyword_import, t.module, t.newline)
+
+
+        def write(t, w):
+            w(t.keyword_import.s)
+            t.module.write(w)
+            w(t.newline)
+
+
+    @share
+    class StatementReturnExpression(Object):
+        __slots__ = ((
+            'keyword_return',           #   String
+            'expression',               #   String
+            'newline',                  #   String
+        ))
+
+
+        def __init__(t, keyword_return, expression, newline):
+            t.keyword_return = keyword_return
+            t.expression     = expression
+            t.newline        = newline
+
+
+        def  __repr__(t):
+            return arrange('<Return %r %r %r>', t.keyword_return, t.expression, t.newline)
+
+
+        def write(t, w):
+            w(t.keyword_return.s)
+            t.expression.write(w)
+            w(t.newline)
---
+++ Sapphire/Token.py
@@ -0,0 +1,30 @@
+#
+#   Copyright (c) 2017 Amit Green.  All rights reserved.
+#
+@gem('Sapphire.Token')
+def gem():
+    @share
+    class Token(Object):
+        __slots__ = ((
+            's',
+        ))
+
+
+        is_keyword = false
+
+
+        def __init__(t, s):
+            t.s = s
+
+
+        def __repr__(t):
+            return arrange('<%s %s>', t.__class__.__name__, portray_string(t.s))
+
+
+        def write(t, w):
+            w(t.s)
+
+
+    @share
+    class UnknownLine(Token):
+        pass
