6516f87d0922bd44d7cf0335c5efbdf5db3abfa5
Amit Green <amit.mixie@gmail.com>
2017-09-08 08:13:10 -0400

    Start parsing list expressions

--- Sapphire/Parse1Atom.py
+++ Sapphire/Parse1Atom.py
@@ -6,6 +6,96 @@ def gem():
     show = 0
 
 
+    def parse1__list_expression__left_square_bracket(left_square_bracket):
+        #
+        #   1
+        #
+        middle_1 = parse1_atom()
+
+
+        #
+        #   TODO: Need to handle a ']' here
+        #
+
+        operator_1 = tokenize_operator()
+
+        if not operator_1.is_end_of_ternary_expression:
+            middle_1 = parse1_ternary_expression__X__any_expression(middle_1, operator_1)
+
+            operator_1 = qk()
+            wk(none)
+
+        if operator_1.is_right_square_bracket:
+            return ListExpression_1(left_square_bracket, middle_1, operator_1)
+
+        if not operator_1.is_comma:
+            raise_unknown_line(1)
+
+        #
+        #   2
+        #
+        middle_2 = tokenize_atom()
+
+        if middle_2.is_right_square_bracket:
+            return ListExpression_1(left_square_bracket, middle_1, Comma_RightSquareBracket(operator_1, middle_2))
+
+        operator_2 = tokenize_operator()
+
+        if not operator_2.is_end_of_ternary_expression:
+            middle_2 = parse1_ternary_expression__X__any_expression(middle_2, operator_2)
+
+            operator_2 = qk()
+            wk(none)
+
+        if operator_2.is_right_square_bracket:
+            return ListExpression_2(left_square_bracket, middle_1, operator_1, middle_2, operator_2)
+
+        if not operator_2.is_comma:
+            raise_unknown_line(2)
+
+        #
+        #   3
+        #
+        middle_3 = tokenize_atom()
+
+        if middle_3.is_right_square_bracket:
+            return ListExpression_2(
+                       left_square_bracket,
+                       middle_1,
+                       operator_1,
+                       middle_2,
+                       Comma_RightSquareBracket(operator_2, middle_3),
+                   )
+
+        many = [left_square_bracket, middle_1, operator_1, middle_2, operator_2]
+
+        while 7 is 7:
+            operator_7 = tokenize_operator()
+
+            if not operator_7.is_end_of_ternary_expression:
+                middle_3 = parse1_ternary_expression__X__any_expression(middle_3, operator_7)
+
+                operator_7 = qk()
+                wk(none)
+
+            many.append(middle_3)
+
+            if operator_7.is_right_square_bracket:
+                many.append(operator_7)
+                return ListExpression_Many(Tuple(many))
+
+            if not operator_7.is_comma:
+                raise_unknown_line(3)
+
+            middle_3 = tokenize_atom()
+
+            if middle_3.is_right_square_bracket:
+                many.append(Comma_RightSquareBracket(operator_7, middle_3))
+                return ListExpression_Many(Tuple(many))
+
+            many.append(operator_7)
+
+
     def parse1__parenthesized_expression__left_parenthesis(left_parenthesis):
         #
         #   1
@@ -26,7 +116,7 @@ def gem():
             wk(none)
 
         if operator_1.is_right_parenthesis:
-            return PathenthesizedExpression(left_parenthesis, middle_1, operator_1)
+            return ParenthesizedExpression(left_parenthesis, middle_1, operator_1)
 
         if not operator_1.is_comma:
             raise_unknown_line(1)
