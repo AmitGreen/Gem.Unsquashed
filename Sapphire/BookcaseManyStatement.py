e1c9dfd9e3da1a0ab9f1a2158703df72dba71ee0
Amit Green <amit.mixie@gmail.com>
2017-10-08 17:18:55 -0400

    Allow comments to be prefixed to Delete_Many

--- Sapphire/BookcaseManyStatement.py
+++ Sapphire/BookcaseManyStatement.py
@@ -6,6 +6,14 @@ def gem():
     require_gem('Sapphire.BookcaseManyExpression')
 
 
+    def write__comment_many(t, w):
+        begin = t.frill.begin
+
+        begin.v.write(w)
+        w(begin.w.s)
+        write__X__many_end(t, w)
+
+
     class AssignStatment_Many(BookcaseManyExpression):
         __slots__           = (())
         display_name        = 'assign-*'
@@ -17,8 +25,6 @@ def gem():
         def add_comment(t, comment):
             frill = t.frill
 
-            my_line('frill.many: %r', frill.many)
-
             return conjure_comment_assign_many(conjure_vw_frill(comment, frill.begin), t.many, frill.many, frill.end)
 
 
@@ -26,8 +32,7 @@ def gem():
             frill = t.frill
             begin = frill.begin
 
-            return arrange('<%s +%d %s %s %s>',
-                           t.display_name,
+            return arrange('<assign-* +%d %s %s %s>',
                            begin.total,
                            ' '.join(v.display_token()   for v in t.many),
                            frill.many.display_token(),
@@ -37,7 +42,7 @@ def gem():
         def dump_token(t, f, newline = true):
             frill = t.frill
 
-            f.partial('<%s +%d ', t.display_name, frill.begin.total)
+            f.partial('<assign-* +%d ', frill.begin.total)
 
             dump_token__X__many(t, f)
 
@@ -63,8 +68,7 @@ def gem():
             frill = t.frill
             begin = frill.begin
 
-            return arrange('<%s +%d %s %s %s %s>',
-                           t.display_name,
+            return arrange('<#assign-* +%d %s %s %s %s>',
                            begin.w.total,
                            begin.v   .display_token(),
                            ' '.join(v.display_token()   for v in t.many),
@@ -87,13 +91,51 @@ def gem():
             return t.frill.begin.w
 
 
-        def write(t, w):
-            begin = t.frill.begin
+        write = write__comment_many
 
-            begin.v.write(w)
-            w(begin.w.s)
-            write__X__many_end(t, w)
 
+    class Comment_DeleteStatement_Many(BookcaseManyExpression):
+        __slots__           = (())
+        display_name        = '#delete-*'
+        is_else_header      = false
+        is_statement        = true
+        is_statement_header = false
+
+
+        @property
+        def indentation(t):
+            return t.frill.begin.w.a
+
+
+        def display_token(t):
+            frill          = t.frill
+            begin          = frill.begin
+            comment        = begin.v
+            indented_token = begin.w
+
+            return arrange('<#delete-* +%d %s %s %s %s %s>',
+                           indented_token.a.total,
+                           indented_token.b.display_token(),
+                           comment         .display_token(),
+                           ' '.join(v.display_token()   for v in t.many),
+                           frill.many.display_token(),
+                           frill.end .display_token())
+
+
+        def dump_token(t, f, newline = true):
+            frill          = t.frill
+            begin          = frill.begin
+            comment        = begin.v
+            indented_token = begin.w
+
+            with f.indent(arrange('<#delete-* +%d ', indented_token.a.total), '>'):
+                comment.dump_token(f)
+                indented_token.b.dump_token(f)
+                dump_token__X__many(t, f)
+                frill.end.dump_token(f)
+
+
+        write = write__comment_many
 
 
     class DeleteStatement_Many(BookcaseManyExpression):
@@ -104,35 +146,46 @@ def gem():
         is_statement_header = false
 
 
+        def add_comment(t, comment):
+            frill = t.frill
+
+            return conjure_comment_delete_many(conjure_vw_frill(comment, frill.begin), t.many, frill.many, frill.end)
+
+
         @property
         def indentation(t):
             return t.frill.begin.a
 
 
         def display_token(t):
-            frill = t.frill
+            frill          = t.frill
+            indented_token = frill.begin
 
-            frill_begin = frill.begin
-
-            return arrange('<delete-* +%d %s %s %s %s>',
-                           frill_begin.v.total,
-                           frill_begin.w.display_token(),
-                           ' '.join(v   .display_token()   for v in t.many),
-                           frill.many   .display_token(),
-                           frill.end    .display_token())
+            return arrange('<delete-* +%d%s %s %s %s %s>',
+                           indented_token.a   .total,
+                           indented_token.b   .display_token(),
+                           ' '.join(v         .display_token()   for v in t.many),
+                           frill         .many.display_token(),
+                           frill         .end .display_token())
 
 
         def dump_token(t, f, newline = true):
-            frill_begin = t.frill.begin
+            frill          = t.frill
+            indented_token = frill.begin
 
-            f.partial('<delete-* +%d ', frill_begin.a.total)
-            frill_begin.b.dump_token(f)
+            f.partial('<delete-* +%d ', indented_token.a.total)
+            indented_token.b.dump_token(f)
 
-            return dump_token__X__many(t, f, newline)
+            dump_token__X__many(t, f)
+
+            r = frill.end.dump_token(f, false)
+
+            return f.token_result(r, newline)
 
 
     conjure_assign_many         = produce_conjure_bookcase_many_expression('assign-*',  AssignStatment_Many)
     conjure_comment_assign_many = produce_conjure_bookcase_many_expression('#assign-*', Comment_AssignStatment_Many)
+    conjure_comment_delete_many = produce_conjure_bookcase_many_expression('#delete-*', Comment_DeleteStatement_Many)
     conjure_delete_many         = produce_conjure_bookcase_many_expression('delete-*',  DeleteStatement_Many)
 
 
